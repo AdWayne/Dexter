3fde0a9a554a8e2db96b7d95310fa0517cb259db
AdWayne <145919719+AdWayne@users.noreply.github.com>
2024-11-29 18:21:09 +0300

    Update scriptDexter.js

--- scriptDexter.js
+++ scriptDexter.js
@@ -100,7 +100,7 @@ const videosBySeason = [
         "https://mega.nz/embed/rVVCXBia#-VdERZUeaUd6Wfwv5PuTEgHD4UE0krHmpKddOKkMCJI",
         "https://mega.nz/embed/WZMgkRbD#X53JFxbAhJWFWahS9oblt870j7r6D1MLw-ArqCe916k",
         "https://mega.nz/embed/TFtWGaQY#8coZaeXBwMc3590PQ-YzsGSOcPJiCkYQKawGWrqAS0E",
-        "",
+        "https://mega.nz/embed/GYlDGI4S#hAbECL9kmK3X-SpMiDyBUfBf6B53x2Epmj9DPcgHoYA",
         "",
         "",
         "",
